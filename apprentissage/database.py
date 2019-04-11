e79ba56d6108d2158eaf15b96bf6f33ffc5e9f68
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-04-11 22:45:42 +0200

    Update database.py

--- apprentissage/database.py
+++ apprentissage/database.py
@@ -3,6 +3,18 @@ from config import *
 
 
         
+class create_base:
+    def database(self):
+        
+        self.connexion = mysql.connector.connect(host=HOST,
+                                                 user=USER,
+                                                 password=PASSWORD)
+
+        self.cursor = self.connexion.cursor()
+        
+        self.cursor.execute("""CREATE DATABASE JOJO""")
+        self.connexion.commit()
+
 class connexion_database:
     def connexion(self):
         
@@ -11,31 +23,97 @@ class connexion_database:
                                                  password=PASSWORD)
         self.cursor = self.connexion.cursor()
 
-
-class create_base:
-    def database(self):
-        connexion_database.connexion(self)
-        self.cursor.execute("""CREATE DATABASE OHPARLO""")
-        
-##create_base = create_base()
-##create_base.database()
-
+        self.cursor.execute("""use JOJO""")
+        self.connexion.commit()
 
 
 class table:
-    def creation_table(self):
+    def creation_table_geo(self):
         connexion_database.connexion(self)
-        self.cursor.execute("""""")
+        self.cursor.execute("""create table forme_geometrique(
+                            carre varchar(100),
+                            rond varchar(100),
+                            rectangle varchar(100));
+                            """)
         self.connexion.commit()
+
+
+
 
 class insertion_table:
     def insertion(self):
         connexion_database.connexion(self)
-
+        
+        self.cursor.execute("""insert into forme_geometrique
+                            (rond) value('circulaire');
+                
+                            """)
+        self.connexion.commit()
 
 class visualisation_table:
     def visualisation(self):
         connexion_database.connexion(self)
 
+        self.cursor.execute("""select * from forme_geometrique""")
+        rows = self.cursor.fetchall()
+        liste = [i for i in rows]
+
+        return liste
+
+
+
+class suppression_table:
+    def suppression(self):
+        connexion_database.connexion(self)
+        
+        self.cursor.execute("""drop table forme_geometrique;
+                            """)
+        self.connexion.commit()
+
+           
+    
+if __name__ == "__main__":
+            
+    create_base = create_base()
+    table = table()
+    insertion_table = insertion_table()
+    suppression_table = suppression_table()
+
+
+    #suppression_table.suppression()
+    
+    #create_base.database()
+
     
 
+    #table.creation_table_geo()
+    #insertion_table.insertion()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
