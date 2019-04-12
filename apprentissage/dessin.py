4eff74b110b7e0f21a6d2338dad4aa601a2eac7e
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-04-12 22:34:39 +0200

    Update dessin.py

--- apprentissage/dessin.py
+++ apprentissage/dessin.py
@@ -1,9 +1,40 @@
 import cv2
-
+from database import *
 #notion de couleur et de taille
 
-class dessin:
 
+
+class dessin:
+    def traitement(self, liste):
+        self.liste = liste
+
+
+        forme = visualisation_table.visualisation(self)
+        #traitement nombre
+
+        c = 0
+        for i in self.liste:
+            
+            if i == []:
+                pass
+            else:
+                if i[0][1] == 'int':#or numeral et traiteùent_pjrase.traitement_nombre
+
+                    for j in forme:
+
+                    
+                        if self.liste[c + 1][0][2] == j[1]:
+  
+                            for i in range(i[0][0]):
+                                dessin.cercle(self)
+                                #coordonée différente
+
+                        #elif fome[1 ou 0] 
+                
+                            
+            c+=1
+
+        
     def ouverture_image(self, image):
         self.image = image
 
@@ -13,17 +44,22 @@ class dessin:
     def lignes(self):
         img = dessin.ouverture_image(self, image)
         cv2.line(img, (0, 0), (264, 100), (0, 0, 255))
+        dessin.image_show(self, img)
 
     def rectangle(self):
         img = dessin.ouverture_image(self, image)
         cv2.rectangle(img, (110, 200), (200, 250), (255, 0, 0), 1)
-
+        dessin.image_show(self, img)
         
     def cercle(self):
-        img = dessin.ouverture_image(self, image)
+        img = cv2.imread("trait.png")
         cv2.circle(img, (180, 130), 20, (0, 255, 0), 1)
-
-
+        
+        cv2.imshow("image.jpg", img)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+        #remettre les import meme si c une faute d'importer
+        
     def image_show(self, image):
         self.image = image
 
