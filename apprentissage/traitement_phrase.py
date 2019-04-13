26cf0e6cf88d575ec8d4e05a3b384e45172cba86
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-04-13 15:22:43 +0200

    Update traitement_phrase.py

--- apprentissage/traitement_phrase.py
+++ apprentissage/traitement_phrase.py
@@ -36,7 +36,7 @@ class traitement_phrase:
 class traitement_reponse:
     def nombre(self, phrase):
 
-
+   
         liste = [[],[],[],[],[],[],[],
                  [],[],[],[],[],[],[],
                  [],[],[],[],[],[],[],
@@ -47,25 +47,28 @@ class traitement_reponse:
         self.phrase = self.phrase.split()
         c = 0
         for i in self.phrase:
+
             try:
                 i = int(i)
                 if i == int(i):
-                    indexage = self.phrase.index(str(i))
-                    liste[indexage].append([int(i), "int"])
+                    liste[c].append([int(i), "int"])
             except:
                 pass
+            finally:
+                c+=1
 
+    
         return liste
 
 
     def nm(self, phrase, liste):
-
+        
         self.liste = liste     
         self.phrase = phrase
 
         self.phrase = self.phrase.split()
 
-
+        c = 0
         for i in self.phrase:
 
             if i[-1] == 's':
@@ -82,13 +85,14 @@ class traitement_reponse:
                 numeral = str(propriete).find("Forme d’adjectif numéral")
 
                 if nm >= 0:
-                    indexage = self.phrase.index(i)
-                    self.liste[indexage].append([j, "Nom"])
+                    self.liste[c].append([j, "Nom"])
+               
                     
                 elif numeral >= 0:
-                    indexage = self.phrase.index(i)
-                    self.liste[indexage].append([j, "numeral"])
+                    self.liste[c].append([j, "numeral"])
 
+                c+=1
+                
 
 
                     
@@ -104,15 +108,16 @@ class traitement_reponse:
                 numeral = str(propriete).find("Forme d’adjectif numéral")
 
                 if nm >= 0:
-                    indexage = self.phrase.index(i)
-                    self.liste[indexage].append([i, "Nom"])
+                    self.liste[c].append([i, "Nom"])
+                 
                     
                 elif numeral >= 0:
-                    indexage = self.phrase.index(i)
-                    self.liste[indexage].append([i, "numeral"])
-
+                    self.liste[c].append([i, "numeral"])
+                   
+                c+=1
                     
-     
+        
+
         return self.liste
 
         #a la fin faire: liste2 = [i for i in liste if i != []]
@@ -123,13 +128,26 @@ class traitement_reponse:
     def traitement_nombre(self, liste):
         self.liste = liste
 
-        dico = {"un":1, "deux":2, "trois":3," quattre":4,
+        dico = {"un":1,"une":1, "deux":2, "trois":3,"quattre":4,
                 "cinq":5, "six":6, "sept":7, "huit":8, "neuf":9,
                 "dix":10}
 
-        
+        c = 0
+        for i in self.liste:
+            if i == []:
+                pass
+            else:
+                if i[0][1] == 'numeral':
+                    for cle, valeur in dico.items():
+                        try:
+                            if i[0][0] == cle:
+                                self.liste[c][0][0] = valeur
+                        except:
+                            pass
+            c+=1
 
-
+        return self.liste
+    
 
 
 
