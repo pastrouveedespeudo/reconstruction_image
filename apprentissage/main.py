d78659c28a619a070d9892a8c4829653aad584e6
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-04-13 15:22:54 +0200

    Update main.py

--- apprentissage/main.py
+++ apprentissage/main.py
@@ -7,10 +7,11 @@ from dessin import *
 #une voiture
 #de quoi est fait une voiture
 #de 4 roues, de 6 vitres, d'une antenne et d'un corps
+
 #une voiture
 #4 roues juxtaposées sur une caisse
 
-
+#2 roues avant 2 roues arrière sur une caisse
 class main:
  
     def question(self):
@@ -28,9 +29,9 @@ class main:
             liste1 = traitement_reponse.nm(self, reponse, liste)
             sens_mot.sens(self, liste1)
             traitement_rep = sens_mot.localisation(self, liste1, split)
-            sens_mot.association_morceau_phrase(self, traitement_rep, split)
+            liste2 = sens_mot.association_morceau_phrase(self, traitement_rep, split)
             
-        dessin.traitement(self, traitement_rep)
+        dessin.traitement(self, liste2)
   
 
 
